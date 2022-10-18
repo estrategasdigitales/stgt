cdd18336e6db1a8da21925c653f7aadfe6396812
brandon10torres <brandon10torres@gmail.com>
2022-10-18 18:09:57 -0500

    Ajustes HTML

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,33 @@ $( document ).ready(function()
 {
     //BOTÓN PARA DESPLEGAR ASIENTOS
     //Varía según el número de filas que se tenga
-    //$('#fila-asientos-ida1').hide();
     $('#btn-fila-asientos-ida1').click(function(){
         $(this).toggleClass('active');
         $('#fila-asientos-ida1').toggleClass('active');
+        $('#fila-asientos-ida2').removeClass('active');
+        $('#fila-asientos-regreso1').removeClass('active');
     });
-    //$('#fila-asientos-ida2').hide();
     $('#btn-fila-asientos-ida2').click(function(){
         $(this).toggleClass('active');
         $('#fila-asientos-ida2').toggleClass('active');
+        $('#fila-asientos-ida1').removeClass('active');
+        $('#fila-asientos-regreso1').removeClass('active');
     });
-    //$('#fila-asientos-regreso1').hide();
     $('#btn-fila-asientos-regreso1').click(function(){
         $(this).toggleClass('active');
         $('#fila-asientos-regreso1').toggleClass('active');
+        $('#fila-asientos-ida1').removeClass('active');
+        $('#fila-asientos-ida2').removeClass('active');
+    });
+    //ABRIR/CERRAR ACORDEÓN DEPENDIENDO DEL QUE SE ABRA
+    $('#accordionIda .accordion-button').click(function()
+    {
+        $('#accordionRegreso .accordion-button').toggleClass('collapsed');
+        $('#accordionRegreso #collapseOneRegreso').toggleClass('show');
+    });
+    $('#accordionRegreso .accordion-button').click(function()
+    {
+        $('#accordionIda .accordion-button').toggleClass('collapsed');
+        $('#accordionIda #collapseOneIda').toggleClass('show');
     });
 });
