97bff36b0b497b090192f7e70c57d40e96798814
Adrian Guerrero <guerreroradrian1111@gmail.com>
2022-10-28 14:07:47 -0500

    Subiendo cambio en combos para vista mobile

--- js/scripts.js
+++ js/scripts.js
@@ -38,5 +38,34 @@ $( document ).ready(function()
     {
         $('.form-viaje-home .fecha-vuelta').toggleClass('active');
     });
+    //Traducción del selector de fecha en home
+     $.datepicker.regional['es'] = {
+        closeText: 'Cerrar',
+        prevText: '< Ant',
+        nextText: 'Sig >',
+        currentText: 'Hoy',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+    //Activar el selector de fecha en home con asignación de icono y formato
+    $( function() {
+        $( ".datepicker" ).datepicker( {
+               dateFormat:"dd-mm-yy", 
+               showOn: "button",
+               buttonImage: "img/ico-input-date.png",
+               buttonImageOnly: true
+              });
+      } );
 
 });
